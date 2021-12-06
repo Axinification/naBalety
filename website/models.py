ea5897c0592b23897c1e392c82bffff20dcb7122
Axinification <Alekwit@gmail.com>
2021-12-06 09:17:09 +0100

    Working event/ticket adding

--- website/models.py
+++ website/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from django.db.models.fields import SlugField
 from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
 
 class Event(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, db_index=True)
     banner = models.ImageField(upload_to='banners/' ,null=True, blank=True)
     description = models.TextField(blank=True)
     address = models.CharField(max_length=255)
-    slug = SlugField(max_length=255, blank=True)
+    slug = models.SlugField(max_length=255, blank=True)
     event_date_time = models.DateTimeField()
     price_1 = models.DecimalField(max_digits=5, decimal_places=2)
     pool_1 = models.IntegerField()
@@ -23,28 +23,18 @@ class Event(models.Model):
 
     def __str__(self):
         return (
-            self.id,
+            f"""{
             self.name,
-            self.banner,
             self.description,
-            self.address,
-            self.slug,
-            self.event_date_time,
-            self.price_1,
-            self.pool_1,
-            self.pool_date_1,
-            self.price_2,
-            self.pool_2,
-            self.pool_date_2,
-            self.price_3,
-            self.pool_3
+            self.address
+            }"""
         )
 
 
 class Ticket(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     event = models.ForeignKey(Event, related_name='ticket', on_delete=models.CASCADE)
-    slug = SlugField(max_length=255, blank=True)
+    slug = models.SlugField(max_length=255, blank=True)
     date_sold = models.DateTimeField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     bought_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name='buyer')
@@ -52,10 +42,12 @@ class Ticket(models.Model):
 
     def __str__(self):
         return (
-            self.id,
-            self.slug,
+            f"""{
+            self.bought_by,
             self.date_sold,
-            self.price
+            self.price,
+            self.promotor
+            }"""
         )
 
 # class User(models.Model):
